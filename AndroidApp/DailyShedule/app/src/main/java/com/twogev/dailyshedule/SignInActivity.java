0e1b2bd15f86517d6a5d13a5fe1aa79e4ff1b78e
1-ПК\Maks <makspel62@gmail.com>
2017-10-23 20:46:44 +0300

    робота з progress bar

--- AndroidApp/DailyShedule/app/src/main/java/com/twogev/dailyshedule/SignInActivity.java
+++ AndroidApp/DailyShedule/app/src/main/java/com/twogev/dailyshedule/SignInActivity.java
@@ -6,12 +6,8 @@
 
 package com.twogev.dailyshedule;
 
-import java.net.URLEncoder;
-import java.io.UnsupportedEncodingException;
-
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
@@ -20,11 +16,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.net.*;
-import java.io.*;
-
 
 public class SignInActivity extends AppCompatActivity {
+
+    EditText edit_login;
+    TextView text_login;
+
+    EditText edit_password;
+    TextView text_password;
+
+    Intent registerA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,20 +39,20 @@ public class SignInActivity extends AppCompatActivity {
         Button but_register = (Button) findViewById(R.id.but_register);
         but_register.setOnClickListener(but_register_Click);
 
-        intent = new Intent(this, RegisterActivity.class);
-    }
+        edit_login = (EditText)findViewById(R.id.edit_login);
+        text_login = (TextView)findViewById(R.id.text_login);
 
-    Intent intent1;
+        edit_password = (EditText)findViewById(R.id.edit_password);
+        text_password = (TextView)findViewById(R.id.text_password);
+
+        registerA = new Intent(this, RegisterActivity.class);
+    }
 
     private SearchView.OnClickListener but_log_in_Click = new SearchView.OnClickListener()
     {
         public void onClick(View v)
         {
-            EditText edit_login = (EditText)findViewById(R.id.edit_login);
-            TextView text_login = (TextView)findViewById(R.id.text_login);
-
-            EditText edit_password = (EditText)findViewById(R.id.edit_password);
-            TextView text_password = (TextView)findViewById(R.id.text_password);
+            boolean incorectInput = false;
             switch (check_login(edit_login.getText().toString()))
             {
                 case (0):
@@ -61,10 +62,12 @@ public class SignInActivity extends AppCompatActivity {
                 case (1):
                     text_login.setTextColor(Color.RED);
                     text_login.setText("Incorect email address");
+                    incorectInput = true;
                     break;
                 case (2):
                     text_login.setTextColor(Color.RED);
                     text_login.setText("Write your email");
+                    incorectInput = true;
                     break;
             }
 
@@ -77,66 +80,31 @@ public class SignInActivity extends AppCompatActivity {
                 case (1):
                     text_password.setTextColor(Color.RED);
                     text_password.setText("Write your password");
+                    incorectInput = true;
                     break;
                 case (2):
                     text_password.setTextColor(Color.RED);
                     text_password.setText("Too short password");
+                    incorectInput = true;
                     break;
             }
 
+            if(incorectInput) return;
+
             //SendRequest();
-            try {
-                String key = URLEncoder.encode("d3J5s66G", "UTF-8");
-                String email = URLEncoder.encode(edit_login.getText().toString(), "UTF-8");
-                String password = URLEncoder.encode(edit_password.getText().toString(), "UTF-8");
-                new DownloadImageTask().execute("https://daytalk.000webhostapp.com/sign_in.php?key="+key+"&email="+email+"&password="+password);
-            }catch (UnsupportedEncodingException e){}
+            ShowWaitingActivity();
             //ShowMain();
         }
     };
 
-    private class DownloadImageTask extends AsyncTask<String, Void, String> {
-        /** The system calls this to perform work in a worker thread and
-         * delivers it the parameters given to AsyncTask.execute() */
-        protected String doInBackground(String... urls) {
-            return SendRequest(urls[0]);
-        }
-
-        /** The system calls this to perform work in the UI thread and delivers
-         * the result from doInBackground() */
-        protected void onPostExecute(String result) {
-            Button but_register = (Button) findViewById(R.id.but_register);
-            but_register.setText(result);
-        }
-    }
-
-    public String SendRequest(String urlAddress)
+    private void ShowWaitingActivity()
     {
-        String SetServerString = "";
-        try {
-            URL url = new URL(urlAddress);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            try {
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                byte[] mas = new byte[100];
-                int len = in.read(mas);
-                for (int i = 0; i < len; i++)
-                {
-                   SetServerString += (char)mas[i];
-                }
-            } finally {
-                urlConnection.disconnect();
-            }
-        }catch (Exception e){SetServerString = "Fail";}
-
-        return SetServerString;
-    }
-
-    public void ShowMain()
-    {
-        intent1 = new Intent(this, Main.class);
-        startActivity(intent1);
-        this.finish();
+        Intent waitingA = new Intent(this, WaitingActivity.class);
+        //waitingA.putExtra("Parent", this.getClass());
+        waitingA.putExtra("Type", 0);
+        waitingA.putExtra("Email", edit_login.getText().toString());
+        waitingA.putExtra("Password", edit_password.getText().toString());
+        startActivity(waitingA);
     }
 
     private int check_login(String s)
@@ -155,13 +123,11 @@ public class SignInActivity extends AppCompatActivity {
         return i;
     }
 
-    Intent intent;
-
     private SearchView.OnClickListener but_register_Click = new SearchView.OnClickListener()
     {
         public void onClick(View v)
         {
-            startActivity(intent);
+            startActivity(registerA);
         }
     };
 
